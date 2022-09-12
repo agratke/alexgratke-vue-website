26d27ae8ed3889692261208a39cf4fa255e38874
agratke <agratke@comcast.net>
2022-09-12 17:59:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-5s8khk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-5s8khk)
---
+++ src/components/AlexGratke.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+</script>
+
+<template>
+
+  <h3>{{msg}}</h3>
+
+  <p>asdfasdf</p>
+</template>
+
+<style scoped>
+
+</style>
