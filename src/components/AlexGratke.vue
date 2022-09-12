b3e478bebb3d9f5c881498cc5394d1421103bcc0
agratke <agratke@comcast.net>
2022-09-12 18:15:21 -0500

    1 binaries uploaded

--- src/components/AlexGratke.vue
+++ src/components/AlexGratke.vue
@@ -8,12 +8,14 @@ defineProps({
 </script>
 
 <template>
-  <img >
+  <img src='../assets/head.jpg' width="450" height="450"/>
   <h3>{{msg}}</h3>
 
   <p></p>
 </template>
 
 <style scoped>
-
+img {
+  border-radius: 50%;
+}
 </style>
