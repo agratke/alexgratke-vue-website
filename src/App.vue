f55a2bc9383e9d1113e7c53e80f68918309c5dc7
agratke <agratke@comcast.net>
2022-09-15 22:26:00 -0500

    added toolbar, icons, styling

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,29 @@ import AlexGratke from './components/AlexGratke.vue'
 </script>
 
 <template>
-  <AlexGratke msg="Alex Gratke"/>
+  <!-- <q-layout view="hHh lpR fFf"> -->
+    <div class="q-pb-md">
+      <q-toolbar class="bg-secondary">
+        <q-toolbar-title>
+          Alex Gratke
+        </q-toolbar-title>
+        <q-tabs v-model="tab" inline-label class="" >
+          <q-tab name="about" icon="person" label="About" />
+          <q-tab name="projects" icon="construction" label="Projects" />
+          <q-tab name="musicCollage" icon="audiotrack" label="What Am I Listening To" />
+        </q-tabs>
+      </q-toolbar>
+    </div>
+
+    <div class='row'>
+      <div class='col-sm-2'></div>
+      <div class='col-sm-8 main-content'>
+        <AlexGratke msg="Alex Gratke"/>
+      </div>
+      <div class='col-sm-2'></div>
+    </div>
+  <!-- </q-layout> -->
+  
 </template>
 
 <style scoped>
